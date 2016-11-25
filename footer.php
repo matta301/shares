ad450a7cc6d7e8f6ba0d1985b912b5d6cbac7b9a
matta301 <matt_a70@hotmail.com>
2016-11-25 20:38:20 +0000

    displaying report titles in sidebar

--- footer.php
+++ footer.php
@@ -1,6 +1,17 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
-    
+    <script type="text/javascript">
+
+			$('.button-collapse').sideNav({
+					menuWidth: 300, // Default is 240
+					edge: 'left', // Choose the horizontal origin
+					closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+					draggable: true // Choose whether you can drag to open on touch screens
+				}
+			);
+
+
+    </script>
 </body>
 </html>
--- report-titles.php
+++ report-titles.php
@@ -1,41 +1,55 @@
 <?php include 'header.php' ;?>  	
+
+	<style type="text/css">
+		.side-nav {
+		    padding: 2em 3em 10em 2em !important;
+		    width: 430px;
+		}
+	</style>
 	
 	<div class="outer-container row">
-	<?php		
+		<ul id="slide-out" class="side-nav fixed">
+			<li><a class="subheader">Reports</a></li>
+			<li><div class="divider"></div></li>
+			<?php		
 
-		$posts = array();
-		
-		$reportType = $crawler->filter('a.facebox')->each(function ($node) {
-
-			// removes (x) from string
-			$titles = preg_split('(\(\d+\))', trim($node->text()));
-			$titles = array_filter($titles);
-			$titles = array_unique($titles);
-
-			return $posts[] = $titles;
-		});
-
-		// Remove duplicate document sub headings
-		$reportType = array_unique($reportType, SORT_REGULAR);
-		
-		
-		// Merges arrays returned from after removing duplicates
-		foreach($reportType as $value) {
-    		$mergedArray[] = array_merge($value);
-		}
-
-		
-		// Loop over arrays to display Report notification headings
-		foreach ($mergedArray as $value) {    			
-			foreach ($value as $key => $title) {
+				$posts = array();
 				
-				echo '<p>' . $title . '</p>';
+				$reportType = $crawler->filter('a.facebox')->each(function ($node) {
 
-			}
-		}	
-	
-	?>
+					// removes (x) from string
+					$titles = preg_split('(\(\d+\))', trim($node->text()));
+					$titles = array_filter($titles);
+					$titles = array_unique($titles);
 
+					return $posts[] = $titles;
+				});
+
+				// Remove duplicate document sub headings
+				$reportType = array_unique($reportType, SORT_REGULAR);
+				
+				
+				// Merges arrays that are returned into 2 and remove duplicates
+				foreach($reportType as $value) {
+		    		$mergedArray[] = array_merge($value);
+				}							
+
+
+				// Loop over arrays to display Report notification headings
+				foreach ($mergedArray as $value) {
+					foreach ($value as $title) {
+						// Push to new empty array so they are all contained within one array
+						//$push = array_push($allTitles, $title);
+						echo '<li>' . $title . '</li>';
+					}
+				}			
+
+
+				
+
+				//echo '<p>' . var_dump($allTitles) . '</p>';	
+			?>
+		</ul>
 	</div><!-- End of row -->
 
 <?php include 'footer.php' ;?>
